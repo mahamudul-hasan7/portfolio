a7e1eb06be82c8f42e1966b15d9270030ad60484
Mahamudul Hasan <mahamud.7info@gmail.com>
2026-06-07 10:44:20 +0600

    Remove remaining Next.js files and app/tsx files

--- experience-portfolio/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import PortfolioExperience from '@/components/PortfolioExperience'
-import { getPortfolioStore } from '@/lib/portfolio-data'
-
-export default async function Home() {
-  const store = await getPortfolioStore()
-  return <PortfolioExperience content={store.content} />
-}
